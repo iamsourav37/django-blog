a5e9663af913a6a16740d495774d820c2fec8f03
iamsourav37 <souravganguly108@gmail.com>
2021-07-08 15:07:28 +0530

    django blog application

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+# Create your views here.
+
+post = [
+    {
+        "author": "Sourav",
+        "title": "Why Django",
+        "content": "django is very powerful framework based on Python",
+        "date_post": "July 08, 2021"
+    },
+    {
+        "author": "Sourav",
+        "title": "Why React",
+        "content": "React is very popular, single page application javascript library",
+        "date_post": "July 08, 2021"
+    },
+]
+
+
+def home(request):
+    context = {
+        "title": "Home",
+        "posts": post
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    context = {
+        "title": "About"
+    }
+    return render(request, "blog/about.html", context)
